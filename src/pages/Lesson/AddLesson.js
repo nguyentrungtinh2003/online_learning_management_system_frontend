bae7e8b7c0a971b2ee9fb48d63060c6c4060707c
NguyenTrongHieu <nguyentronghieu14032003@gmail.com>
2025-03-18 23:03:41 +0700

    SCRUM-212 <Axios Post Lesson>

--- src/pages/Lesson/AddLesson.js
+++ src/pages/Lesson/AddLesson.js
@@ -1,18 +1,26 @@
+import { ToastContainer,toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import AdminNavbar from "../../components/Navbar/AdminNavbar";
 import { FaBuffer } from "react-icons/fa";
 import { MdNavigateNext } from "react-icons/md";
 
 const AddLesson = () => {
+  // State lưu dữ liệu từ form nhập vào
+  const navigate = useNavigate();
+
+  const {courseId, id} = useParams();
   const [lessonData, setLessonData] = useState({
     lessonName: "",
     description: "",
     date: "",
-    courseId: 1, // ID của khóa học
+    course : {id : courseId} , // ID của khóa học
     isDeleted: false,
   });
+  const [loading, setLoading] = useState(false); // Trạng thái loading
+
   const [img, setImg] = useState(null);
   const [video, setVideo] = useState(null);
 
@@ -30,6 +38,8 @@ const AddLesson = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+
     const formData = new FormData();
     formData.append(
       "lesson",
@@ -45,10 +55,25 @@ const AddLesson = () => {
         { headers: { "Content-Type": "multipart/form-data" } }
       );
       console.log("Thành công:", response.data);
-      alert("Thêm bài học thành công!");
+      // alert("Thêm bài học thành công!");
+      toast.success("Thêm bài học thành công!", {
+      position: "top-right",
+      autoClose: 3000,  // 4 giây
+      });
+
+      setTimeout(() => {
+        navigate(-1);
+        }, 4000);    
     } catch (error) {
       console.error("Lỗi:", error.response?.data || error.message);
-      alert("Lỗi khi thêm bài học!");
+      // alert("Lỗi khi thêm bài học!");
+       toast.error("Không thể thêm bài học!", {
+              position: "top-right",
+              autoClose: 3000, 
+            });
+    }
+    finally {
+      setLoading(false);
     }
   };
 
@@ -108,32 +133,25 @@ const AddLesson = () => {
               className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-scolor"
             ></textarea>
           </div>
-          <div className="flex items-center space-x-4">
-            <label className="w-1/4 text-gray-700 font-medium">Date:</label>
-            <input
-              type="datetime-local"
-              name="date"
-              value={lessonData.date}
-              onChange={handleChange}
-              className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-scolor"
-            />
-          </div>
         </div>
         <div className="flex justify-end space-x-2 mt-6">
-          <Link
-            to="/admin/courses/:id/lesson"
+          <button
+            onClick={() => navigate(-1)}
             className="px-6 py-2 border-2 border-sicolor text-ficolor rounded-lg hover:bg-opacity-80"
           >
             Cancel
-          </Link>
+          </button>
           <button
             type="submit"
-            className="px-6 py-2 bg-scolor text-ficolor rounded-lg hover:bg-opacity-80"
+            className={`px-6 py-2 rounded-lg ${loading ? "bg-gray-400" : "bg-scolor text-ficolor hover:bg-opacity-80"}`}
+            disabled={loading}
           >
-            Submit
+            {loading ? "Processing..." : "Submit"}
           </button>
         </div>
       </form>
+            <ToastContainer /> 
+      
     </div>
   );
 };
