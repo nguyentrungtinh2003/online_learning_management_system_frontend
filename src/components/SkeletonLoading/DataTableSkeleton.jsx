c9080b82019c2d1ae5c199bbc4a75978b8049743
VanTan22ab <izukumi5672@gmail.com>
2025-05-01 20:45:17 +0700

    Edit Skeleton Loading

--- src/components/SkeletonLoading/DataTableSkeleton.jsx
+++ src/components/SkeletonLoading/DataTableSkeleton.jsx
@@ -4,51 +4,49 @@ export default function DataTableSkeleton() {
   return (
     <tr className="text-center">
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={40} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={40} />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={100} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={100} />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={150} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={150} />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton
-          animation="pulse"
-          variant="rectangular"
-          width={32}
-          height={32}
-        />
+        <div className="flex justify-center">
+          <Skeleton
+            animation="pulse"
+            variant="rectangular"
+            width={60}
+          />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={60} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={60} />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={80} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={80} />
+        </div>
       </td>
       <td className="p-2">
-        <Skeleton animation="pulse" variant="text" width={60} />
+        <div className="flex justify-center">
+          <Skeleton animation="pulse" variant="text" width={60} />
+        </div>
       </td>
       <td className="p-2">
         <div className="flex justify-center gap-1">
-          <Skeleton
-            animation="pulse"
-            variant="circular"
-            width={24}
-            height={24}
-          />
-          <Skeleton
-            animation="pulse"
-            variant="circular"
-            width={24}
-            height={24}
-          />
-          <Skeleton
-            animation="pulse"
-            variant="circular"
-            width={24}
-            height={24}
-          />
+          <Skeleton animation="pulse" variant="circular" width={24} height={24} />
+          <Skeleton animation="pulse" variant="circular" width={24} height={24} />
+          <Skeleton animation="pulse" variant="circular" width={24} height={24} />
         </div>
       </td>
     </tr>
